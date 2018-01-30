92068bbcdeb5082af39f97f0feed3022918a17c9
lrx <liangrx@gzhhit.com>
2018-01-30 18:08:01 +0800

    增加图片，调整loading位置

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { NavBar, Picker, Icon, ActivityIndicator } from 'antd-mobile';
 import axios from 'axios';
+import hospitalImg from '../../img/hospital.jpg';
 
 let hospitals
 class Index extends Component {
@@ -53,7 +54,7 @@ class Index extends Component {
         return this.state.init ? 
         (<div>
             <NavBar mode="light"><span onClick={this.showPicker}>{this.state.title}</span><Icon type="down"/></NavBar>
-                首页
+            <img src={hospitalImg} alt="医院" width="100%"/>
             <Picker
             visible = {this.state.pickerVisible}
             title="选择医院"
@@ -64,7 +65,7 @@ class Index extends Component {
             onOk={this.handleOk}
             onDismiss={() => this.setState({ pickerVisible: false })}
             />
-        </div>) : <ActivityIndicator size="large" text="加载中..." /> ;
+        </div>) :  <div style={{ height: '100%', display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}><ActivityIndicator size="large" /></div>  ;
     }
 }
 
