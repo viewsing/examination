165336f70b6ceb7d15626a04ac35b6f6a1524f0e
lrx <liangrx@gzhhit.com>
2018-03-29 13:55:39 +0800

    修改mock_server，设置了credentials；

--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
 const Koa = require('koa');
 const cors = require('koa2-cors');
 const router = require('koa-router')();
+const path = require('path');
 const fs = require('fs');
 
 const server = new Koa();
 
-server.use(cors());
+server.use(cors({
+    credentials: true //客户端设置了withCredentials，允许设置cookies
+}));
 
-let routesMap = JSON.parse(fs.readFileSync('./routes.json', 'utf-8'));
+let routesMap = JSON.parse(fs.readFileSync(path.join(__dirname, './routes.json'), 'utf-8'));
 
 //读取routes.json文件
 for ( let route in routesMap) {
     router.all(route, async function(ctx, next) {
         ctx.response.type = 'application/json';
-        let body = fs.readFileSync(routesMap[route], 'utf-8');
+        let body = fs.readFileSync(path.join(__dirname, routesMap[route]), 'utf-8');
         ctx.response.body = body;
     });
 }
@@ -21,7 +24,7 @@ for ( let route in routesMap) {
 
 //请求日志
 server.use(async (ctx, next) => {
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}`);
     await next();
 });
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 // window.USERURL = 'http://211.159.189.178:8080/exApp/';
-window.USERURL = '/exApp/';
-// window.USERURL = 'http://localhost:3002/hr_examination_user/';
-window.developing = false;
+// window.USERURL = '/exApp/';
+window.USERURL = 'http://localhost:3008/exApp/';
+window.developing = true;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
